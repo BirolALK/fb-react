f5018212953395701153e95ee40bc8842c4cc1d4
BirolAlkan <birolalkan93@gmail.com>
2022-11-24 23:58:05 +0300

    v

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -9,7 +9,6 @@ const Card = ({name, number, photo, position}) => {
                  <h1 className="name">{name}</h1>
                  <h3 className="number">{number}</h3>
                  <p className="position">{position}</p>
-                 
               </div>
             </div> 
         </div>
